57a7afae69e7494632f2653ad4275cb754df1085
Vladimir Poplavskij <float45@gmail.com>
2012-05-29 09:47:04 +0300

    gzip param edit

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-      //include('gzip_top.php');
+      //include('gzip.php');
       if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) ob_start("ob_gzhandler"); else ob_start();
       $version='1.0';
       $features='
@@ -172,12 +172,14 @@ opacity:\$op;}","switch (\$this->browser['name']){
 			}else{
 			      \$str='';
 			}"));
+			/*
 			$info=date ("D, d M Y H:i:s.", filemtime("index.php"));
 			header("Expires: Mon, 1 Jan 1970 05:00:00 GMT");// дата в прошлом
 			header("Last-Modified: " . $info . " GMT");  // всегда модифицируется
 			header("Cache-Control: no-store, no-cache, must-revalidate");// HTTP/1.1
 			header("Cache-Control: post-check=0, pre-check=0", false);
 			header("Pragma: no-cache");// HTTP/1.0
+			*/
 ?>
 <!DOCTYPE html>
 <html lang="en">
