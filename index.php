bac85f641d6fb06a05a816f4957ee69e17d2c01d
Vladimir Poplavskij <float45@gmail.com>
2012-05-29 23:39:51 +0300

    header info

--- index.php
+++ index.php
@@ -172,14 +172,14 @@ opacity:\$op;}","switch (\$this->browser['name']){
 			}else{
 			      \$str='';
 			}"));
-			/*
+			
 			$info=date ("D, d M Y H:i:s.", filemtime("index.php"));
 			header("Expires: Mon, 1 Jan 1970 05:00:00 GMT");// дата в прошлом
 			header("Last-Modified: " . $info . " GMT");  // всегда модифицируется
 			header("Cache-Control: no-store, no-cache, must-revalidate");// HTTP/1.1
 			header("Cache-Control: post-check=0, pre-check=0", false);
 			header("Pragma: no-cache");// HTTP/1.0
-			*/
+			
 ?>
 <!DOCTYPE html>
 <html lang="en">
